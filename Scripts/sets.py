bf719de9acd6459562667c2a7fb4dda1139f0e28
Quinten <90308786+QNoah@users.noreply.github.com>
2026-03-16 15:15:34 +0100

    Added requirements and added functions for python

--- Scripts/sets.py
+++ Scripts/sets.py
@@ -1,12 +1,25 @@
 import json
-
-with open('./json/sets.json', 'r') as file:
-    data = json.load(file)
+import psycopg2
 
 
+def make_connection():
+    connection = psycopg2.connect(
+        database="scalpcentral",
+        user="postgres",
+        password="",
+        host="127.0.0.1",  # Verander dit naar de host van de externe server.
+        port=5432,
+    )
+    if connection.closed != 0:
+        print("Connectie werkt niet.")
+        return
+    json_load()
 
 
+def json_load():
+    with open("./json/sets.json", "r") as file:
+        data = json.load(file)
+    with open("demofile.txt", "w") as f:
+        f.write(json.dumps(data, indent = 4))
 
-
-# with open("demofile.txt", "w") as f:
-#     f.write(json.dumps(data, indent = 4))
+make_connection()

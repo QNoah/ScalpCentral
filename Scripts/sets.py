05b1d9a6007a7797b0d90ac958a1943ef756aa51
Quinten <90308786+QNoah@users.noreply.github.com>
2026-03-16 14:21:51 +0100

    Kleine syntax voor json

---
+++ Scripts/sets.py
@@ -0,0 +1,12 @@
+import json
+
+with open('./json/sets.json', 'r') as file:
+    data = json.load(file)
+
+
+
+
+
+
+# with open("demofile.txt", "w") as f:
+#     f.write(json.dumps(data, indent = 4))

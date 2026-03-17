97c4566d9ca1dbbacb38f4ac17780bac7b718deb
dabboloosefun <146081681+dabboloosefun@users.noreply.github.com>
2026-03-17 19:58:49 +0100

    help UTF* be ubgging
    Co-authored-by: Quinten <QNoah@users.noreply.github.com>

--- Scripts/cards.py
+++ Scripts/cards.py
@@ -1,6 +1,9 @@
 import json
 import psycopg2
 import os
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
+
 
 
 def get_connection():
@@ -12,6 +15,10 @@ def get_connection():
             host="127.0.0.1",
             port=5432,
         )
+        connection.set_client_encoding('UTF8')
+        with connection.cursor() as cur:
+            cur.execute("""SHOW client_encoding;""")
+            print(cur.fetchone())
         return connection
     except Exception as e:
         print("Connectie werkt niet:", e)
@@ -22,127 +29,90 @@ def create_tables(connection):
     cursor = connection.cursor()
     schema_sql = """
 CREATE TABLE IF NOT EXISTS attacks(
-    id INT NOT NULL,
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    damage VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+    damage VARCHAR(255),
+    description TEXT,
+    CONSTRAINT unique_attacks_only UNIQUE NULLS NOT DISTINCT (name, damage, description)
 );
 
 CREATE TABLE IF NOT EXISTS abilities(
-    id INT NOT NULL,
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    type VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+    type VARCHAR(255),
+    description TEXT,
+    CONSTRAINT unique_abilities_only UNIQUE NULLS NOT DISTINCT (name, type, description)
 );
 
 CREATE TABLE IF NOT EXISTS rules(
-    id INT NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    description TEXT UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS card_images(
-    id INT NOT NULL,
-    small VARCHAR(255),
-    large VARCHAR(255),
-    PRIMARY KEY (id)
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    small TEXT,
+    large TEXT
 );
 
 CREATE TABLE IF NOT EXISTS types(
-    id INT NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS subtypes(
-    id INT NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS cards (
-    id VARCHAR(255) NOT NULL,
-    set_id VARCHAR(255) NOT NULL,
+    id VARCHAR(255) UNIQUE NOT NULL PRIMARY KEY,
+    set_id VARCHAR(255) NOT NULL REFERENCES sets(id),
     name VARCHAR(255) NOT NULL,
     supertype VARCHAR(255) NOT NULL,
     hp INT,
     evolves_from_name VARCHAR(255),
     artist VARCHAR(255),
     rarity VARCHAR(255),
-    flavor_text VARCHAR(255),
-    images_id INT NOT NULL,
-    soft_delete BOOLEAN NOT NULL,
-    PRIMARY KEY (id),
-    CONSTRAINT fk_cards_set_id
-        FOREIGN KEY (set_id) REFERENCES sets(id),
-    CONSTRAINT fk_cards_images_id
-        FOREIGN KEY (images_id) REFERENCES card_images(id)
+    flavor_text TEXT,
+    images_id BIGINT NOT NULL REFERENCES card_images(id),
+    soft_delete BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE IF NOT EXISTS cards_to_evolutions(
-    id INT NOT NULL,
-    card_id VARCHAR(255) NOT NULL,
+    card_id VARCHAR(255) NOT NULL REFERENCES cards(id),
     evolves_to_name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id),
-    CONSTRAINT fk_cards_to_evolutions_card_id
-        FOREIGN KEY (card_id) REFERENCES cards(id)
+    PRIMARY KEY (card_id, evolves_to_name)
 );
 
 CREATE TABLE IF NOT EXISTS cards_to_abilities(
-    id INT NOT NULL,
-    card_id VARCHAR(255) NOT NULL,
-    ability_id INT NOT NULL,
-    PRIMARY KEY (id),
-    CONSTRAINT fk_cards_to_abilities_card_id
-        FOREIGN KEY (card_id) REFERENCES cards(id),
-    CONSTRAINT fk_cards_to_abilities_ability_id
-        FOREIGN KEY (ability_id) REFERENCES abilities(id)
+    card_id VARCHAR(255) NOT NULL REFERENCES cards(id),
+    ability_id BIGINT NOT NULL REFERENCES abilities(id),
+    PRIMARY KEY (card_id, ability_id)
 );
 
 CREATE TABLE IF NOT EXISTS cards_to_attacks(
-    id INT NOT NULL,
-    card_id VARCHAR(255) NOT NULL,
-    attack_id INT NOT NULL,
-    PRIMARY KEY (id),
-    CONSTRAINT fk_cards_to_attacks_card_id
-        FOREIGN KEY (card_id) REFERENCES cards(id),
-    CONSTRAINT fk_cards_to_attacks_attack_id
-        FOREIGN KEY (attack_id) REFERENCES attacks(id)
+    card_id VARCHAR(255) NOT NULL REFERENCES cards(id),
+    attack_id BIGINT NOT NULL REFERENCES attacks(id),
+    PRIMARY KEY (card_id, attack_id)
 );
 
 CREATE TABLE IF NOT EXISTS cards_to_rules(
-    id INT NOT NULL,
-    card_id VARCHAR(255) NOT NULL,
-    rule_id INT NOT NULL,
-    PRIMARY KEY (id),
-    CONSTRAINT fk_cards_to_rules_card_id
-        FOREIGN KEY (card_id) REFERENCES cards(id),
-    CONSTRAINT fk_cards_to_rules_rule_id
-        FOREIGN KEY (rule_id) REFERENCES rules(id)
+    card_id VARCHAR(255) NOT NULL REFERENCES cards(id),
+    rule_id BIGINT NOT NULL REFERENCES rules(id),
+    PRIMARY KEY (card_id, rule_id)
 );
 
 CREATE TABLE IF NOT EXISTS cards_to_types(
-    id INT NOT NULL,
-    card_id VARCHAR(255) NOT NULL,
-    type_id INT NOT NULL,
-    PRIMARY KEY (id),
-    CONSTRAINT fk_cards_to_types_card_id
-        FOREIGN KEY (card_id) REFERENCES cards(id),
-    CONSTRAINT fk_cards_to_types_type_id
-        FOREIGN KEY (type_id) REFERENCES types(id)
+    card_id VARCHAR(255) NOT NULL REFERENCES cards(id),
+    type_id BIGINT NOT NULL REFERENCES types(id),
+    PRIMARY KEY (card_id, type_id)
 );
 
 CREATE TABLE IF NOT EXISTS cards_to_subtypes(
-    id INT NOT NULL,
-    card_id VARCHAR(255) NOT NULL,
-    subtype_id INT NOT NULL,
-    PRIMARY KEY (id),
-    CONSTRAINT fk_cards_to_subtypes_card_id
-        FOREIGN KEY (card_id) REFERENCES cards(id),
-    CONSTRAINT fk_cards_to_subtypes_subtype_id
-        FOREIGN KEY (subtype_id) REFERENCES subtypes(id)
+    card_id VARCHAR(255) NOT NULL REFERENCES cards(id),
+    subtype_id BIGINT NOT NULL REFERENCES subtypes(id),
+    PRIMARY KEY (card_id, subtype_id)
 );
 """
     cursor.execute(
@@ -151,14 +121,17 @@ CREATE TABLE IF NOT EXISTS cards_to_subtypes(
     connection.commit()
     cursor.close()
 
-def load_cards_database(cards):
+def load_cards_database(cards, connection):
+    cursor = connection.cursor()
+
     for c in cards:
         c_id = c["id"]
-        print(c_id)
         c_set_id = (c["id"].split("-")[0])
         c_name = c["name"]
+        print(c_name)
         c_supertype = c["supertype"]
-        c_subtype = c.get("subtypes")
+        c_subtypes = c.get("subtypes")
+        c_types = c.get("types")
         c_hp = c.get("hp")
         c_evolves_from_name = c.get("evolvesFrom")
         c_attacks = c.get("attacks")
@@ -170,13 +143,49 @@ def load_cards_database(cards):
         c_imagesmall = c["images"]["small"]
         c_imagelarge = c["images"]["large"]
 
-        
-        
+        cursor.execute("""INSERT INTO card_images (small, large) VALUES (%s, %s) RETURNING id""", (c_imagesmall, c_imagelarge))
+        images_id = cursor.fetchone()[0]
 
-def json_load():
+        # cursor.execute(
+        #                 """
+        #                 INSERT INTO public.cards (id, set_id, name, supertype, hp, evolves_from_name, artist, rarity, flavor_text, images_id, soft_delete)
+        #                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, DEFAULT)
+        #                 """, (c_id, c_set_id, c_name, c_supertype, c_hp, c_evolves_from_name, c_artist, c_rarity, c_flavor_text, images_id)
+        # )
+
+        # if c_attacks:
+        #     for attack in c_attacks:
+        #         cursor.execute("""INSERT INTO attacks (name, damage, description) VALUES (%s, %s, %s) ON CONFLICT ON CONSTRAINT unique_attacks_only DO UPDATE SET name = EXCLUDED.name RETURNING id;""", (attack["name"], attack.get("damage"), attack.get("text")))
+        #         attack_id = cursor.fetchone()[0]
+        #         cursor.execute("""INSERT INTO cards_to_attacks (card_id, attack_id) VALUES (%s, %s) ON CONFLICT DO NOTHING""", (c_id, attack_id))
+
+        # if c_abilities:
+        #     for ability in c_abilities:
+        #         cursor.execute("""INSERT INTO abilities (name, type, description) VALUES (%s, %s, %s) ON CONFLICT ON CONSTRAINT unique_abilities_only DO UPDATE SET name = EXCLUDED.name RETURNING id;""", (ability["name"], ability.get("type"), ability.get("text")))
+        #         ability_id = cursor.fetchone()[0]
+        #         cursor.execute("""INSERT INTO cards_to_abilities (card_id, ability_id) VALUEs (%s, %s) ON CONFLICT DO NOTHING""", (c_id, ability_id))
+
+        # if c_rules:
+        #     for rule in c_rules:
+        #         cursor.execute("""INSERT INTO rules (description) VALUES (%s) ON CONFLICT (description) DO UPDATE SET description = EXCLUDED.description RETURNING id;""", (rule,))
+        #         rule_id = cursor.fetchone()[0]
+        #         cursor.execute("""INSERT INTO cards_to_rules (card_id, rule_id) VALUES (%s, %s) ON CONFLICT DO NOTHING""", (c_id, rule_id))
+
+        # if c_subtypes:
+        #     for subtype in c_subtypes:
+        #         cursor.execute("""INSERT INTO subtypes (name) VALUES (%s) ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name RETURNING id;""", (subtype,))
+        #         subtype_id = cursor.fetchone()[0]
+        #         cursor.execute("""INSERT INTO cards_to_subtypes (card_id, subtype_id) VALUES (%s, %s) ON CONFLICT DO NOTHING""", (c_id, subtype_id))
+        # if c_types:
+        #     for type in c_types:
+        #         cursor.execute("""INSERT INTO types (name) VALUES (%s) ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name RETURNING id""", (type,))
+        #         type_id = cursor.fetchone()[0]
+        #         cursor.execute("""INSERT INTO cards_to_types (card_id, type_id) VALUES (%s, %s) ON CONFLICT DO NOTHING""", (c_id, type_id))
+
+def json_load(connection):
     cards_folder = "./json/cards"
     for file in os.listdir(cards_folder):
-        if not file.endswith(".json"): ## DIT MOET OOIT VERANDERD WORDEN > .JSON ALLEEN
+        if not file.endswith("base1.json"): ## DIT MOET OOIT VERANDERD WORDEN > ".json"
             continue
 
         path = os.path.join(cards_folder, file)
@@ -185,24 +194,20 @@ def json_load():
             with open(path, encoding="utf-8") as f:
                 cards = json.load(f)
                 if cards:
-                    load_cards_database(cards)
+                    load_cards_database(cards, connection)
                     print("Success")
                         
         except Exception as e:
-            print(f"Error in {file}: {e}")
+            print(f"Error in {file}: {str(e).encode('utf-8', errors='ignore').decode()}")
 
 if __name__ == "__main__":         
     connection = get_connection()
     if connection == False:
         exit()
-    # create_tables(connection)
-    # connection.commit()
-    json_load()
+    create_tables(connection)
+    json_load(connection)
+    connection.commit()
     connection.close()
-    print()
+
+
     print("Import klaar.")
-
-# INSERT INTO "public"."cards" ("id", "set_id", "name", "supertype", "hp", "evolves_from_name", "artist", "rarity", "flavor_text", "soft_delete") VALUES
-# ('base1 - 1', 'base1', 'doodo', 'Ex', 100, 'dreoleo', 'artruur', 'scar', 'poop plug in the toilet', 'FALSE');
-
-# SELECT * FROM cards JOIN sets ON cards.set_id = sets.id WHERE cards.id = 'base1 - 1'

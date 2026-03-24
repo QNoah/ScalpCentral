f81ecbbafb503ef93939464a9ccb32ff931d646e
dabboloosefun <146081681+dabboloosefun@users.noreply.github.com>
2026-03-24 13:26:17 +0100

    Create tables done, NOTE: pase REFERENCES user(id) aan op user table type

---
+++ Scripts/orderContents.py
@@ -0,0 +1,51 @@
+import psycopg2
+import sys
+
+sys.stdout.reconfigure(encoding="utf-8")
+
+def get_connection():
+    try:
+        connection = psycopg2.connect(
+            database="scalpcentral",
+            user="postgres",
+            password="",
+            host="127.0.0.1",
+            port=5432,
+        )
+        connection.set_client_encoding("UTF8")
+        with connection.cursor() as cur:
+            cur.execute("""SHOW client_encoding;""")
+            print(cur.fetchone())
+            cur.execute("""SHOW server_encoding;""")
+            print(cur.fetchone())
+        return connection
+    except Exception as e:
+        print("Connectie werkt niet:", e)
+        return False
+
+
+def create_tables(connection):
+    cursor = connection.cursor()
+    schema_sql = """
+CREATE TABLE IF NOT EXISTS order_contents(
+    order_id BIGINT REFERENCES orders(id) NOT NULL,
+    product_id BIGINT REFERENCES products(id) NOT NULL,
+    amount INT NOT NULL,
+    PRIMARY KEY (order_id, product_id)
+);
+"""
+    cursor.execute(schema_sql)
+    cursor.close()
+
+def run(connection):
+    create_tables(connection)
+    connection.commit()
+    print(f"ordersContents.py ran succesfully.")
+    return
+
+if __name__ == "__main__":
+    connection = get_connection()
+    if connection is False:
+        print("Error: Connection is False")
+        sys.exit(1)
+    run(connection)

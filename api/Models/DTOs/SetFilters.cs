57e65a25c66faad866502b41743be4ed3099a932
Quinten <90308786+QNoah@users.noreply.github.com>
2026-04-21 13:24:44 +0200

    Set filters toegevoegd

---
+++ api/Models/DTOs/SetFilters.cs
@@ -0,0 +1,6 @@
+public class SetFilters
+{
+    public string Name { get; set; } = string.Empty;
+    public string Series { get; set; } = string.Empty;
+    public DateTime ReleaseDate {get; set;}
+}

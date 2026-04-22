d5b78ac72d0a6173f3b497766eb1e2845585a9d8
dabboloosefun <146081681+dabboloosefun@users.noreply.github.com>
2026-04-22 13:50:27 +0200

    forgot injection in service

--- api/Repository/Product/IProductRepository.cs
+++ api/Repository/Product/IProductRepository.cs
@@ -2,7 +2,6 @@ namespace ScalpCentral.Api.Repository;
 
 public interface IProductRepository
 {
-    Task<List<ProductModel>> GetPaged(int page, int pagesize);
     Task<List<ProductModel>> GetFiltered(ProductFilter filter);
     Task<ProductModel?> GetById(long id);
     Task<long> Create(ProductModel product);

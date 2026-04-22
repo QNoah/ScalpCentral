5bb3db5fa6295dc0817c0f881a310af3c04d50d0
Quinten <90308786+QNoah@users.noreply.github.com>
2026-04-22 15:12:30 +0200

    Fixed repo, service, controller

--- api/Models/Webshop/SetModel.cs
+++ api/Models/Webshop/SetModel.cs
@@ -1,9 +1,9 @@
 public class SetModel
 {
-    public required string Id {get; set;}
-    public required string Name {get; set;}
-    public required int Series {get; set;}
-    public required int TotalCards {get; set;}
-    public required int ReleaseDate {get; set;}
-    public required string? ImageLogo {get; set;}
+    public required string Id { get; set; }
+    public required string Name { get; set; }
+    public required string Series { get; set; }
+    public required int TotalCards { get; set; }
+    public required DateOnly ReleaseDate { get; set; }
+    public required string? ImageLogo { get; set; }
 }

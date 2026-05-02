a1f34dc37adfa5fb83eead3949013d7de0ca8e92
dabboloosefun <146081681+dabboloosefun@users.noreply.github.com>
2026-05-02 18:06:28 +0200

    surface level

--- Website/src/Components/Pages/SearchResults.tsx
+++ Website/src/Components/Pages/SearchResults.tsx
@@ -248,7 +248,7 @@ export function SearchResults() {
                         </select>
                     </div>
                     <div className="results-grid">
-                        {searchResults.map((result: Product) => ProductCard(result))}
+                        {searchResults.map(result => ProductCard(result))}
                     </div>
                     <div className="pagination">
                         {renderButtons()}

bbef97028d2ad05e4ae37464dc558623a517907e
Quinten <90308786+QNoah@users.noreply.github.com>
2026-05-06 15:27:06 +0200

    small styling adjustment

--- Website/src/Components/Utils/Button.tsx
+++ Website/src/Components/Utils/Button.tsx
@@ -1,5 +1,5 @@
 import type { IButton } from "../Types/IButton";
 
 export default function Button(ButtonProps: IButton) {
-  return <a className={`${ButtonProps.ButtonColor} ${ButtonProps.TextColor} rounded-lg py-2 w-80 font-inter font-md block`}>{ButtonProps.Message}</a>;
+  return <a className={`${ButtonProps.ButtonColor} ${ButtonProps.TextColor} rounded-lg py-2 w-80 font-inter font-md block cursor-pointer`}>{ButtonProps.Message}</a>;
 }

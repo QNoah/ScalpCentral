e6251e656be632924c8ff4687c3abf735c5a8733
dabboloosefun <146081681+dabboloosefun@users.noreply.github.com>
2026-05-06 15:09:38 +0200

    THE GREAT FIGMA CSS PURGE

--- Website/src/Components/Pages/SearchResults.tsx
+++ Website/src/Components/Pages/SearchResults.tsx
@@ -200,62 +200,63 @@ export function SearchResults() {
 
     
     return (
-        <div className="container-search">
-            <Navbar />
-            <div className="search-content">
-                <div className="sidebar">
-                    <h2 className="sidebar-header">FILTERS</h2>
-                    <div className="filters">
-                        <form className="filter-type">
-                            <p>TYPE: {types.length}</p>
-                            {types.map((type: string) => TypeOption(type))}
-                        </form>
-                        <form className="filter-set">
-                            <p>SET: {sets.length}</p>
-                            {sets.map((set: string) => SetOption(set))}
-                        </form>
-                        <form className="filter-series">
-                            <p>SERIES: {series.length}</p>
-                            {series.map((serie: string) => SeriesOption(serie))}
-                        </form>
-                        <p>MIN PRICE</p>
-                        <input className="filter-minPrice" type="number" placeholder="0" onChange={
-                            (e) => setMinPrice(e.currentTarget.value ? parseInt(e.currentTarget.value) : null)
-                        } />
-                        <p>MAX PRICE</p>
-                        <input className="filter-maxPrice" type="number" placeholder="-" onChange={
-                            (e) => setMaxPrice(e.currentTarget.value ? parseInt(e.currentTarget.value) : null)
-                        } />
-                        <p>ON SALE</p>
-                        <input className="filter-onSale" type="checkbox" name="On Sale" onChange={
-                            (e) => setOnSale(e.currentTarget.checked)
-                        } />
-                        <p>IN STOCK</p>
-                        <input className="filter-inStock" type="checkbox" name="In Stock" onChange={
-                            (e) => setInStock(e.currentTarget.checked)
-                        } />
-                        <button className="apply-filters-button" onClick={applyFilters}>APPLY FILTERS</button>
-                        <button className="clear-filters-button" onClick={clearFilters}>CLEAR FILTERS</button>
+        <main>
+            <div className="container-search">
+                <Navbar />
+                <div className="search-content">
+                    <div className="sidebar">
+                        <h2 className="sidebar-header">FILTERS</h2>
+                        <div className="filters">
+                            <form className="filter-type">
+                                <p>TYPE: {types.length}</p>
+                                {types.map((type: string) => TypeOption(type))}
+                            </form>
+                            <form className="filter-set">
+                                <p>SET: {sets.length}</p>
+                                {sets.map((set: string) => SetOption(set))}
+                            </form>
+                            <form className="filter-series">
+                                <p>SERIES: {series.length}</p>
+                                {series.map((serie: string) => SeriesOption(serie))}
+                            </form>
+                            <p>MIN PRICE</p>
+                            <input className="filter-minPrice" type="number" placeholder="0" onChange={
+                                (e) => setMinPrice(e.currentTarget.value ? parseInt(e.currentTarget.value) : null)
+                            } />
+                            <p>MAX PRICE</p>
+                            <input className="filter-maxPrice" type="number" placeholder="-" onChange={
+                                (e) => setMaxPrice(e.currentTarget.value ? parseInt(e.currentTarget.value) : null)
+                            } />
+                            <p>ON SALE</p>
+                            <input className="filter-onSale" type="checkbox" name="On Sale" onChange={
+                                (e) => setOnSale(e.currentTarget.checked)
+                            } />
+                            <p>IN STOCK</p>
+                            <input className="filter-inStock" type="checkbox" name="In Stock" onChange={
+                                (e) => setInStock(e.currentTarget.checked)
+                            } />
+                            <button className="apply-filters-button" onClick={applyFilters}>APPLY FILTERS</button>
+                            <button className="clear-filters-button" onClick={clearFilters}>CLEAR FILTERS</button>
+                        </div>
                     </div>
-                </div>
-                <div className="results-content">
-                    <div className="results-header">
-                        <h2 className="results-count">{totalCount} RESULTS</h2>
-                        <select className="sort-dropdown" onChange={handleSortChange}>
-                            <option value="default">DEFAULT</option>
-                            <option value="priceLowHigh">PRICE: LOW TO HIGH</option>
-                            <option value="priceHighLow">PRICE: HIGH TO LOW</option>
-                        </select>
-                    </div>
-                    <div className="results-grid">
-                        {searchResults.map(result => ProductCard(result))}
-                    </div>
-                    <div className="pagination">
-                        {renderButtons()}
+                    <div className="results-content">
+                        <div className="results-header">
+                            <h2 className="results-count">{totalCount} RESULTS</h2>
+                            <select className="sort-dropdown" onChange={handleSortChange}>
+                                <option value="default">DEFAULT</option>
+                                <option value="priceLowHigh">PRICE: LOW TO HIGH</option>
+                                <option value="priceHighLow">PRICE: HIGH TO LOW</option>
+                            </select>
+                        </div>
+                        <div className="results-grid">
+                            {searchResults.map(result => ProductCard(result))}
+                        </div>
+                        <div className="pagination">
+                            {renderButtons()}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-
+        </main>
     )
 }

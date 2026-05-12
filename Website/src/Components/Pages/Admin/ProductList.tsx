2f88a881f69928c5d0d6df92b459a1849de306a0
Quinten <90308786+QNoah@users.noreply.github.com>
2026-05-12 14:56:22 +0200

    Added initial sort for the product view list

--- Website/src/Components/Pages/Admin/ProductList.tsx
+++ Website/src/Components/Pages/Admin/ProductList.tsx
@@ -1,59 +1,55 @@
 import { Paper } from "@mui/material";
 import { DataGrid, type GridColDef } from '@mui/x-data-grid';
 import Navbar from "../../Utils/Navbar";
-
+import type { Product } from "../../Types/Product";
+import { useState, useEffect } from 'react';
 
 const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 20 },
-  { field: 'firstName', headerName: 'First name', flex: 1},
-  { field: 'lastName', headerName: 'Last name',  flex: 1},
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    flex: 1
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    flex: 1,
-    valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
-  },
+  { field: 'id', headerName: 'ID', flex: 1},
+  { field: 'name', headerName: 'Naam', flex: 2 },
+  { field: 'price', headerName: 'Prijs', flex: 1, type: 'number' },
 ];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+export default function ProductList() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
 
-const paginationModel = { page: 0, pageSize: 5 };
+  useEffect(() => {
+    const fetchProducts = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const response = await fetch("http://localhost:5231/api/products/");
+        const data = await response.json();
+        setProducts(data.result ?? data);
+      } catch (err: any) {
+        setError("Error gathering products.");
+      } finally {
+        setLoading(false);
+      }
+    };
 
+    fetchProducts();
+  }, []);
 
-export default function ProductList(){
-    return(
-        <div>
-            <Navbar/>
-            <div className="flex justify-center mt-20">
-               <Paper sx={{ height: 400, width: '80%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        // initialState={{ pagination: { paginationModel } }}
-        // pageSizeOptions={[5, 10]}
-        checkboxSelection
-        // sx={{ border: 0 }}
-      />
-    </Paper>
-        </div>
-        </div>
-    )
+  return (
+    <div>
+      <Navbar />
+      <div className="flex justify-center mt-20" style={{maxHeight: 750}}>
+        <Paper sx={{ width: '80%'}}>
+          <DataGrid
+            rows={products}
+            columns={columns}
+            loading={loading}
+            getRowId={(row) => row.id}
+            initialState={{sorting: {sortModel: [{field: 'id', sort: 'asc'}]}}}
+            // sortingOrder={row}
+            checkboxSelection
+          />
+          {error && <div style={{ color: "red", padding: 16 }}>{error}</div>}
+        </Paper>
+      </div>
+    </div>
+  );
 }

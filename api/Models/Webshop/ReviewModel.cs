2a1109cca54e4608a938b179eb3bde00e0d0256f
dabboloosefun <146081681+dabboloosefun@users.noreply.github.com>
2026-05-27 12:37:12 +0200

    repo almost finsihed

--- api/Models/Webshop/ReviewModel.cs
+++ api/Models/Webshop/ReviewModel.cs
@@ -2,12 +2,12 @@ using ScalpCentral.Api.Models;
 
 public class ReviewModel
 {
-    public int Id {get; set;}
-    public decimal Stars {get; set;}
-    public string? Title {get; set;}
-    public string? Description {get; set;}
-    public DateTime? CreatedAt {get; set;}
+    public int Id { get; set; }
+    public decimal Stars { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public DateTime? CreatedAt { get; set; }
 
-    public required UserModel User {get; set;}
-    public required ProductModel Product {get; set;}
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
 }
--- api/Repository/Review/IReviewRepository.cs
+++ api/Repository/Review/IReviewRepository.cs
@@ -4,11 +4,9 @@ public interface IReviewRepository
 {
     Task<List<ReviewModel>> GetFiltered(ReviewFilter filter);
     Task<PagedResults<ReviewModel>> GetPaged(ReviewFilter filter, int limit);
-    Task<Dictionary<string, string[]>> GetFilters(ReviewFilter filter);
 
     Task<ReviewModel?> GetById(long id);
-    Task<long> Create(CreateProductDto product);
+    Task<long> Create(ReviewDTO review);
+    Task<long> Update(ReviewDTO review, long id);
     Task Delete(int id);
-    Task<long> Update(ReviewModel product);
-    public Task<List<ReviewModel>> GetAllById(List<int> ids);
 }

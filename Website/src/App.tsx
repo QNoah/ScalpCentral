4af8071a461b8d32b8cbdcf3cda008ff71a09e5b
Jeffrey Yeh <yehspam@outlook.com>
2026-06-16 17:22:14 +0200

    fixed profile page

--- Website/src/App.tsx
+++ Website/src/App.tsx
@@ -13,6 +13,7 @@ import Footer from './Components/PageParts/Footer';
 import ProductEdit from './Components/Pages/Admin/ProductEdit';
 import { ResetPasswordPage } from './Components/Pages/ResetPasswordPage';
 import UserList from './Components/Pages/Admin/UserList';
+import ProfilePage from './Components/Pages/ProfilePage';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -61,6 +62,7 @@ function App() {
         <Route path='users-list' element={<UserList/>}/>
         <Route path='product-list' element={<ProductList/>}/>
         <Route path='/reset-password' element={<ResetPasswordPage/>}/>
+        <Route path='/profile' element={<ProfilePage/>}/>
         <Route path="*" element={<p>Page not found</p>} />
         </Routes>
       </main>
